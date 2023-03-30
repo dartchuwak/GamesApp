f6a238564a37d02311e83cb9c4965a46e9d9983f
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-03-31 00:10:25 +0300

    Initial Commit

---
+++ GamesApp/GamesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesAppApp.swift
+//  GamesApp
+//
+//  Created by Evgenii Mikhailov on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GamesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
